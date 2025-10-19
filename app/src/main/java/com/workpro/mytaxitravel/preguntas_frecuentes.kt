e2edab68417fe0eee314e137f1ca07b0abae706c
S109shiro <sebgonrod@gmail.com>
2025-10-19 17:06:03 -0500

    preguntas frecuentes

---
+++ app/src/main/java/com/workpro/mytaxitravel/preguntas_frecuentes.kt
@@ -0,0 +1,20 @@
+package com.workpro.mytaxitravel
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class preguntas_frecuentes : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_preguntas_frecuentes)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
