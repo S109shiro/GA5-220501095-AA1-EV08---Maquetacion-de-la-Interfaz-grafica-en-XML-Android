96bcba36def0cde463ee08821e2282ff899f9d46
S109shiro <sebgonrod@gmail.com>
2025-10-19 14:16:49 -0500

    configuracion perfil

---
+++ app/src/main/java/com/workpro/mytaxitravel/configuracion_perfil.kt
@@ -0,0 +1,20 @@
+package com.workpro.mytaxitravel
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class configuracion_perfil : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_configuracion_perfil)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
